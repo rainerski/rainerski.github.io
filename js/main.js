4a0ea85f49730373ff2d57045a677674292a1947
omesb <36600384+omesb@users.noreply.github.com>
2020-04-04 23:01:25 +0200

    service worker init

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/rainersskicountdown/service-worker.js', {scope: '/rainersskicountdown'})
+    navigator.serviceWorker.register('/service-worker.js')
         .then((reg) => {
             // registration worked
             console.log('Registration succeeded.');
